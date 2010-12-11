738f0af05a65ee83bc82d0812f184587da17eee8
miros <mirosm@mirosm.ru>
2010-12-11 22:31:22 +0300

    bug fixed

--- ext/bsearch/bsearch.c
+++ ext/bsearch/bsearch.c
@@ -5,7 +5,7 @@ static VALUE binary_search(VALUE, VALUE);
 
 void Init_bsearch()
 {
-  rb_define_method(rb_cArray, "binary_search", binary_search, 1);
+  rb_define_method(rb_cArray, "bsearch", binary_search, 1);
 }
 
 static VALUE binary_search(VALUE self, VALUE rb_str_to_search)
@@ -28,7 +28,7 @@ static VALUE binary_search(VALUE self, VALUE rb_str_to_search)
 		if (strcmp(current_value, str_to_search) == 0) {
 			printf("found index: %d \n", index);
 			return INT2NUM(index);
-		} else if(strcmp(current_value, str_to_search) > 0) {
+		} else if(strcmp(current_value, str_to_search) < 0) {
 			printf("> \n");
 			lower = index + 1;
 		} else {
