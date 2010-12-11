94d1a3621bbeaa155577d3c71581abd53869b063
miros <mirosm@mirosm.ru>
2010-12-11 22:24:07 +0300

    binary search for strings is working

--- ext/bsearch/bsearch.c
+++ ext/bsearch/bsearch.c
@@ -1,12 +1,44 @@
 #include <ruby.h>
+#include <string.h>
 
-static VALUE hello_world(VALUE mod)
-{
-  return rb_str_new2("hello world");
-}
+static VALUE binary_search(VALUE, VALUE);
 
 void Init_bsearch()
 {
-  VALUE mBSearch = rb_define_module("Bsearch");
-  rb_define_singleton_method(mBSearch, "hello_world", hello_world, 0);
+  rb_define_method(rb_cArray, "binary_search", binary_search, 1);
+}
+
+static VALUE binary_search(VALUE self, VALUE rb_str_to_search)
+{
+	int upper = RARRAY_LEN(self) - 1;
+	printf("array size: %d \n", upper);
+	int lower = 0;
+
+	VALUE *array = RARRAY_PTR(self);
+	char *str_to_search = rb_string_value_ptr(&rb_str_to_search);
+
+	while(upper >= lower) {
+                
+		int index = lower + (upper - lower) / 2;
+		VALUE *current_element = array + index;
+		printf("current index: %d \n", index);
+		char *current_value = rb_string_value_ptr(current_element);
+		printf("current value: %s \n", current_value);
+
+		if (strcmp(current_value, str_to_search) == 0) {
+			printf("found index: %d \n", index);
+			return INT2NUM(index);
+		} else if(strcmp(current_value, str_to_search) > 0) {
+			printf("> \n");
+			lower = index + 1;
+		} else {
+			printf("< \n");
+			upper = index - 1;
+		}
+               
+	}
+
+	printf("not found");
+
+	return Qfalse;
 }
---
+++ ext/bsearch/test.rb
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + "/bsearch.so"
+require "ap"
+
+ap ["a", "b", "c", "d", "e", "f", "g", "h"].binary_search("d")
+
+
